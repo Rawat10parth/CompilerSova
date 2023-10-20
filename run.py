a1571b2ece5d6e47faf3dec9f94d3b889f119438
Parth Rawat <parthpriyanshu10@gmail.com>
2023-10-21 01:49:50 +0530

    Added Files

---
+++ run.py
@@ -0,0 +1,31 @@
+import lexical as le
+
+# Specify the file path
+file_path = "LEXSAMPLE.txt"
+
+# Initialize an empty list to store the lines from the file
+lines = []
+
+try:
+    # Open the file in read mode
+    with open(file_path, 'r') as file:
+        # Iterate through each line in the file
+        for line in file:
+            # Append the line to the list, removing the newline character
+            lines.append(line.strip())
+
+    # Print the lines or perform any further processing
+    for text in lines:
+        print(text)
+
+        result, error = le.run('<stdin>', text)
+
+        if error:
+            print(error.as_string())
+        else:
+            print(result)
+
+except FileNotFoundError:
+    print(f"File '{file_path}' not found.")
+except Exception as e:
+    print(f"An error occurred: {str(e)}")
